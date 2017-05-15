bf48eac1d60cab69703c28fc4868e5acc0f3c166
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2017-05-15 17:25:05 -0500

    Added secrets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,5 +18,4 @@ Rails.application.configure do
 
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
