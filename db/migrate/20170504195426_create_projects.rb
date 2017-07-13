d0486cfa68761752ee38a7a2014863d66409a165
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2017-07-13 12:02:42 -0500

    Fix typo in migration

--- db/migrate/20170504195426_create_projects.rb
+++ db/migrate/20170504195426_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
-      t.string :slug, null: :false
+      t.string :slug, null: false
       t.index :slug, unique: true
     end
   end
